dc20b8697e2b424c8ea5a1e9ed2bf664a632ab04
renadh12 <renadhc@gmail.com>
2025-12-19 13:05:19 -0600

    Optimized: removed timer overhead, stable P99 latency under load

--- src/main/java/com/pricing/config/RedisConfig.java
+++ src/main/java/com/pricing/config/RedisConfig.java
@@ -1,11 +1,11 @@
 package com.pricing.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.pricing.model.PriceData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,8 +18,11 @@ public class RedisConfig {
         RedisTemplate<String, PriceData> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         
-        Jackson2JsonRedisSerializer<PriceData> serializer = new Jackson2JsonRedisSerializer<>(PriceData.class);
-        serializer.setObjectMapper(new ObjectMapper());
+        // Configure ObjectMapper properly
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        Jackson2JsonRedisSerializer<PriceData> serializer = 
+            new Jackson2JsonRedisSerializer<>(objectMapper, PriceData.class);
         
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
